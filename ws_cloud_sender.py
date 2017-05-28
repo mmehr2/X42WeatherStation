c2dfa46906228932c2c5e6a4b9793250c7d09ccc
Michael L. Mehr <mike@azuresults.com>
2017-05-28 16:46:10 -0700

    Added Apache Kafka producer for cloud access, including test scripts. Solved problem with access errors.

---
+++ ws_cloud_sender.py
@@ -0,0 +1,70 @@
+'''
+Cloud send functions.
+These are based on Apache Kafka Producer for compatibility with 30454 Class Final Project.
+Default topic is: x42ws.public.data
+
+Resources:
+   sudo pip install kafka-python
+
+Date: May 24. 2017
+Author: Michael L. Mehr
+'''
+
+import sys
+import settings as dbs
+
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+default_port = 9092
+default_host = 'localhost'
+default_retries = 3
+default_topic = "x42ws.public.data"
+
+def init():
+    global producer, port, host, retries
+    port = dbs.get('AWS_Port')
+    host = dbs.get('AWS_Instance')
+    retries = dbs.get('AWS_Retries')
+    if port == 'null':
+        port = default_port
+    if host == 'null':
+        host = default_host
+    if retries == 'null':
+        retries = default_retries
+    server1 = '%s:%s' % (host, port)
+    print "Server requested:", server1, "retries", retries
+    result = ""
+    try:
+        producer = KafkaProducer(bootstrap_servers=[server1],\
+                                 retries=retries,\
+                                 value_serializer=lambda m: json.dumps(m).encode('ascii'))
+    except KafkaError as e:
+        result = "Kafka Producer Init Error %s" % e
+    return result
+
+def send(data, topic = default_topic):
+    global producer
+    result = ""
+    try:
+        producer.send(topic, data) # returns an async future
+        producer.flush() # block until all async messages are sent
+    except KafkaError as e:
+        result = "Kafka Producer Send Error %s" % e
+    return result
+
+if __name__=='__main__':
+    global port, host, retries
+    print "Starting Apache Kafka Producer with the following configuration:"
+    result = init()
+    print "  Host:", host
+    print "  Port:", port
+    print "  Retries:", retries
+    if result != "":
+        print "Could not start producer: result:", result
+        exit(255)
+    message = {"test1": 12, "test2": 34}
+    print "Sending message X to topic:", default_topic, " X:", json.stringify(message)
+    result = send(message)
+    print "Result of test send was :", result
+    exit(0)
