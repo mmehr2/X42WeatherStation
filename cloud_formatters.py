ce2d84cd32e568efa80141c88296c519fbab0d3b
Michael L. Mehr <mike@azuresults.com>
2017-06-06 13:45:49 -0700

    Modified to send latitude, longitude, altitude, and reply-to address from settings database to cloud.

--- cloud_formatters.py
+++ cloud_formatters.py
@@ -1,24 +1,33 @@
 import datetime
+import settings as dbs
 
 def make_json_x42(temp, press, humid, light, utc_sample_time):
     '''Creates a JSON object as a Python dictionary from an X42WS data sample.'''
+    lat = dbs.get("X42_Latitude")
+    long = dbs.get("X42_Longitude")
+    alt = dbs.get("X42_Altitude")
     result = {\
         "temperature": temp,
         "pressure": press,
         "humidity": humid,
         "ambient_light": light,
-        "timestamp": "%sZ" % utc_sample_time.isoformat()\
+        "timestamp": "%sZ" % utc_sample_time.isoformat(),\
+        "latitude": "%s" % lat,\
+        "longitude": "%s" % long,\
+        "altitude": "%s" % alt\
         }
     return result
 
 def package(data):
     '''Takes a JSON data object as a Python dictionary. Wraps it in the proper headers.'''
+    reply_url = dbs.get("Ngrok_API")
     result = {\
           "guid": "0-ZZZ123456785B",\
           "destination": "0-AAA12345678",\
           "eventTime": "%sZ" % (datetime.datetime.utcnow().isoformat()),\
           "payload": {\
              "format": "urn:com:azuresults:x42ws:sensors",\
+             "reply-to": "%s" % reply_url,\
              "data": data \
            }\
         }
--- send_email.py
+++ send_email.py
@@ -16,47 +16,47 @@ def init():
 
 def get_ngrok_notice(ws_addr, api_addr):
     body = "Please use the following addresses for X42WS weather station access:\n"
-    body += "\tWeb Server:\thttps://%s.ngrok.io\n" % ws_addr
-    body += "\tREST API Server:\thttps://%s.ngrok.io/weather/api\n" % api_addr
-    body += "\t\tMeasurement:\thttps://%s.ngrok.io/weather/api/sensors\n" % api_addr
-    body += "\t\tLatest data:\thttps://%s.ngrok.io/weather/api/sensors/latest\n" % api_addr
-    body += "\t\tLast 15 minutes of data:\thttps://%s.ngrok.io/weather/api/sensors/latest/15\n" % api_addr
-    body += "\t\tRelay ON:\tcurl -i -H \"Content-Type: application/json\" -X POST https://%s.ngrok.io/weather/api/led/0/1\n" % api_addr
-    body += "\t\tRelay OFF:\tcurl -i -H \"Content-Type: application/json\" -X POST https://%s.ngrok.io/weather/api/led/0/0\n" % api_addr
-    body += "\t\tTake picture:\tcurl -i -H \"Content-Type: application/json\" -X POST https://%s.ngrok.io/weather/api/camera/0/3\n" % api_addr
+    body += "\tWeb Server:\t%s\n" % ws_addr
+    body += "\tREST API Server:\t%s\n" % api_addr
+    body += "\t\tMeasurement:\t%s/sensors\n" % api_addr
+    body += "\t\tLatest data:\t%s/sensors/latest\n" % api_addr
+    body += "\t\tLast 15 minutes of data:\t%s/sensors/latest/15\n" % api_addr
+    body += "\t\tRelay ON:\tcurl -i -H \"Content-Type: application/json\" -X POST %s/led/0/1\n" % api_addr
+    body += "\t\tRelay OFF:\tcurl -i -H \"Content-Type: application/json\" -X POST %s/led/0/0\n" % api_addr
+    body += "\t\tTake picture:\tcurl -i -H \"Content-Type: application/json\" -X POST %s/camera/0/3\n" % api_addr
     body += "Sincerely,\n"
     body += "\n"
     body += "The Management\n"
     return body
  
-def send_mail(toaddr, subject, body):
+def send_mail(toaddr_list, subject, body):
     global fromaddr, password
-    #fromaddr = "michael.mehr.ucsc@gmail.com"
     msg = MIMEMultipart()
     msg['From'] = fromaddr
-    msg['To'] = toaddr
+    msg['To'] = toaddr_list
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(fromaddr, password)
     text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
+    server.sendmail(fromaddr, toaddr_list, text)
     server.quit()
 
 if __name__=="__main__":
     argc = len(sys.argv)
-    wscode = sys.argv[1] if argc>1 else ""
-    restcode = sys.argv[2] if argc>2 else ""
-    nosend = sys.argv[3] if argc>3 else ""
-    if wscode == "":
-        print "Usage: send_email.py XXXXXX YYYYYY [NS]"
-        print "  where XXXXX is the ngrok port 80 client hex code, and"
-        print "  where YYYYY is the ngrok port 8080 client hex code."
+    nosend = sys.argv[1] if argc>1 else ""
+    if nosend == "":
+        print "Usage: send_email.py [S|NS]"
+        print "  Sends email about ngrok port 80 and 8080 client addresses (different each powerup)."
+        print "  Email parameters are stored in the settings database."
+        print "  The S flag, if present, will generate and send the email."
         print "  The NS flag, if present, suppresses the actual send for debugging."
         exit(0)
     init()
-    body = get_ngrok_notice(wscode, restcode)
+    wsaddr = dbs.get('Ngrok_Web')
+    apiaddr = dbs.get('Ngrok_API')
+    body = get_ngrok_notice(wsaddr, apiaddr)
     toaddr = dbs.get('Ngrok_Mailto')
     subject = "New X42WS Addresses"
     print "Email to:", toaddr
@@ -64,5 +64,5 @@ if __name__=="__main__":
     print "Email subject:", subject
     print "Email body:"
     print body
-    if nosend == "":
+    if nosend == 'S' or nosend == 's':
         send_mail(toaddr, subject, body)
